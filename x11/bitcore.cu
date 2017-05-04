3c6158887ea7b0591337fe4747c4f0c6d8d3c2c5
elbandi <bandispam@gmail.com>
2017-05-04 19:42:36 +0200

    bitcore algo with pregenerated permutations

--- x11/bitcore.cu
+++ x11/bitcore.cu
@@ -32,11 +32,14 @@ extern "C" {
 #include "cuda_x11.h"
 
 static uint32_t *d_hash[MAX_GPUS];
+static uint32_t permutations[HASH_FUNC_COUNT_PERMUTATIONS] = {
+	#include "timetravel-permutations.h"
+};
 
 enum Algo {
-	BLAKE = 0,
-	BMW,
-	GROESTL,
+//	BLAKE = 0,
+//	BMW,
+	GROESTL = 0,
 	SKEIN,
 	JH,
 	KECCAK,
@@ -44,88 +47,14 @@ enum Algo {
 	CUBEHASH,
 	SHAVITE,
 	SIMD,
-#if HASH_FUNC_COUNT > 10
-	ECHO,
-#endif
 	MAX_ALGOS_COUNT
 };
-
-inline void swap8(uint8_t *a, uint8_t *b)
-{
-	uint8_t t = *a;
-	*a = *b;
-	*b = t;
-}
-
-inline void initPerm(uint8_t n[], int count)
-{
-	for (int i = 0; i < count; i++)
-		n[i] = i;
-}
-
-static int nextPerm(uint8_t n[], int count)
-{
-	int tail, i, j;
-
-	if (count <= 1)
-		return 0;
-
-	for (i = count - 1; i>0 && n[i - 1] >= n[i]; i--);
-	tail = i;
-
-	if (tail > 0) {
-		for (j = count - 1; j>tail && n[j] <= n[tail - 1]; j--);
-		swap8(&n[tail - 1], &n[j]);
-	}
-
-	for (i = tail, j = count - 1; i<j; i++, j--)
-		swap8(&n[i], &n[j]);
-
-	return (tail != 0);
-}
-
-static void getAlgoString(char *str, int seq)
-{
-	uint8_t algoList[HASH_FUNC_COUNT];
-	char *sptr;
-
-	initPerm(algoList, HASH_FUNC_COUNT);
-
-	for (int k = 0; k < seq; k++) {
-		nextPerm(algoList, HASH_FUNC_COUNT);
-	}
-
-	sptr = str;
-	for (int j = 0; j < HASH_FUNC_COUNT; j++) {
-		if (algoList[j] >= 10)
-			sprintf(sptr, "%c", 'A' + (algoList[j] - 10));
-		else
-			sprintf(sptr, "%u", (uint32_t) algoList[j]);
-		sptr++;
-	}
-	*sptr = '\0';
-}
-
-static __thread uint32_t s_ntime = 0;
-static uint32_t s_sequence = UINT32_MAX;
-static uint8_t s_firstalgo = 0xFF;
-static char hashOrder[HASH_FUNC_COUNT + 1] = { 0 };
 
 #define INITIAL_DATE HASH_FUNC_BASE_TIMESTAMP
 static inline uint32_t getCurrentAlgoSeq(uint32_t ntime)
 {
 	// unlike x11evo, the permutation changes often (with ntime)
 	return (uint32_t) (ntime - INITIAL_DATE) % HASH_FUNC_COUNT_PERMUTATIONS;
-}
-
-// To finish...
-static void get_travel_order(uint32_t ntime, char *permstr)
-{
-	uint32_t seq = getCurrentAlgoSeq(ntime);
-	if (s_sequence != seq) {
-		getAlgoString(permstr, seq);
-		s_sequence = seq;
-	}
 }
 
 // CPU Hash
@@ -143,89 +72,62 @@ extern "C" void bitcore_hash(void *output, const void *input)
 	sph_cubehash512_context  ctx_cubehash1;
 	sph_shavite512_context   ctx_shavite1;
 	sph_simd512_context      ctx_simd1;
-#if HASH_FUNC_COUNT > 10
-	sph_echo512_context      ctx_echo1;
-#endif
 
-	if (s_sequence == UINT32_MAX) {
-		uint32_t *data = (uint32_t*) input;
-		const uint32_t ntime = (opt_benchmark || !data[17]) ? (uint32_t) time(NULL) : data[17];
-		get_travel_order(ntime, hashOrder);
-	}
+	uint32_t *data = (uint32_t*)input;
+	const uint32_t ntime = (opt_benchmark || !data[17]) ? (uint32_t)time(NULL) : data[17];
+	const uint32_t sequence = permutations[getCurrentAlgoSeq(ntime)];
 
-	void *in = (void*) input;
-	int size = 80;
+	sph_blake512_init(&ctx_blake);
+	sph_blake512(&ctx_blake, input, 80);
+	sph_blake512_close(&ctx_blake, hash);
 
-	const int hashes = (int) strlen(hashOrder);
+	sph_bmw512_init(&ctx_bmw);
+	sph_bmw512(&ctx_bmw, hash, 64);
+	sph_bmw512_close(&ctx_bmw, hash);
 
-	for (int i = 0; i < hashes; i++)
+	for (int i = 0; i < (4 * (HASH_FUNC_COUNT - 2)); i += 4)
 	{
-		const char elem = hashOrder[i];
-		uint8_t algo = elem >= 'A' ? elem - 'A' + 10 : elem - '0';
-
-		if (i > 0) {
-			in = (void*) hash;
-			size = 64;
-		}
-
-		switch (algo) {
-		case BLAKE:
-			sph_blake512_init(&ctx_blake);
-			sph_blake512(&ctx_blake, in, size);
-			sph_blake512_close(&ctx_blake, hash);
-			break;
-		case BMW:
-			sph_bmw512_init(&ctx_bmw);
-			sph_bmw512(&ctx_bmw, in, size);
-			sph_bmw512_close(&ctx_bmw, hash);
-			break;
+		switch ((sequence >> i) & 0xf) {
 		case GROESTL:
 			sph_groestl512_init(&ctx_groestl);
-			sph_groestl512(&ctx_groestl, in, size);
+			sph_groestl512(&ctx_groestl, hash, 64);
 			sph_groestl512_close(&ctx_groestl, hash);
 			break;
 		case SKEIN:
 			sph_skein512_init(&ctx_skein);
-			sph_skein512(&ctx_skein, in, size);
+			sph_skein512(&ctx_skein, hash, 64);
 			sph_skein512_close(&ctx_skein, hash);
 			break;
 		case JH:
 			sph_jh512_init(&ctx_jh);
-			sph_jh512(&ctx_jh, in, size);
+			sph_jh512(&ctx_jh, hash, 64);
 			sph_jh512_close(&ctx_jh, hash);
 			break;
 		case KECCAK:
 			sph_keccak512_init(&ctx_keccak);
-			sph_keccak512(&ctx_keccak, in, size);
+			sph_keccak512(&ctx_keccak, hash, 64);
 			sph_keccak512_close(&ctx_keccak, hash);
 			break;
 		case LUFFA:
 			sph_luffa512_init(&ctx_luffa1);
-			sph_luffa512(&ctx_luffa1, in, size);
+			sph_luffa512(&ctx_luffa1, hash, 64);
 			sph_luffa512_close(&ctx_luffa1, hash);
 			break;
 		case CUBEHASH:
 			sph_cubehash512_init(&ctx_cubehash1);
-			sph_cubehash512(&ctx_cubehash1, in, size);
+			sph_cubehash512(&ctx_cubehash1, hash, 64);
 			sph_cubehash512_close(&ctx_cubehash1, hash);
 			break;
 		case SHAVITE:
 			sph_shavite512_init(&ctx_shavite1);
-			sph_shavite512(&ctx_shavite1, in, size);
+			sph_shavite512(&ctx_shavite1, hash, 64);
 			sph_shavite512_close(&ctx_shavite1, hash);
 			break;
 		case SIMD:
 			sph_simd512_init(&ctx_simd1);
-			sph_simd512(&ctx_simd1, in, size);
+			sph_simd512(&ctx_simd1, hash, 64);
 			sph_simd512_close(&ctx_simd1, hash);
 			break;
-#if HASH_FUNC_COUNT > 10
-		case ECHO:
-			sph_echo512_init(&ctx_echo1);
-			sph_echo512(&ctx_echo1, in, size);
-			sph_echo512_close(&ctx_echo1, hash);
-			break;
-#endif
 		}
 	}
 
@@ -251,14 +153,8 @@ extern "C" int scanhash_bitcore(int thr_id, struct work* work, uint32_t max_nonc
 
 	if (opt_benchmark) pdata[17] = swab32(0x59090909);
 
-	if (opt_debug || s_ntime != pdata[17] || s_sequence == UINT32_MAX) {
-		uint32_t ntime = swab32(work->data[17]);
-		get_travel_order(ntime, hashOrder);
-		s_ntime = pdata[17];
-		if (opt_debug && !thr_id) {
-			applog(LOG_DEBUG, "timetravel10 hash order %s (%08x)", hashOrder, ntime);
-		}
-	}
+	uint32_t ntime = swab32(work->data[17]);
+	uint32_t sequence = permutations[getCurrentAlgoSeq(ntime)];
 
 	if (opt_benchmark)
 		ptarget[7] = 0x5;
@@ -286,9 +182,6 @@ extern "C" int scanhash_bitcore(int thr_id, struct work* work, uint32_t max_nonc
 		if (x11_simd512_cpu_init(thr_id, throughput) != 0) {
 			return 0;
 		}
-#if HASH_FUNC_COUNT > 10
-		x11_echo512_cpu_init(thr_id, throughput);
-#endif
 		CUDA_CALL_OR_RET_X(cudaMalloc(&d_hash[thr_id], (size_t) 64 * throughput), -1);
 		CUDA_CALL_OR_RET_X(cudaMemset(d_hash[thr_id], 0, (size_t) 64 * throughput), -1);
 
@@ -301,76 +194,54 @@ extern "C" int scanhash_bitcore(int thr_id, struct work* work, uint32_t max_nonc
 	for (int k=0; k < 19; k++)
 		be32enc(&endiandata[k], pdata[k]);
 
+	// first and second algo seems locked to blake+bmw in bitcore, fine!
+	quark_blake512_cpu_setBlock_80(thr_id, endiandata);
 	cuda_check_cpu_setTarget(ptarget);
 
-	const int hashes = (int) strlen(hashOrder);
-	const char first = hashOrder[0];
-	const uint8_t algo80 = first >= 'A' ? first - 'A' + 10 : first - '0';
-	if (algo80 != s_firstalgo) {
-		s_firstalgo = algo80;
-	}
-
-	// first algo seems locked to blake in bitcore, fine!
-	quark_blake512_cpu_setBlock_80(thr_id, endiandata);
-
 	do {
+		int order = 0;
+
 		// Hash with CUDA
+		quark_blake512_cpu_hash_80(thr_id, throughput, pdata[19], d_hash[thr_id]); order++;
+		TRACE("blake  :");
+		quark_bmw512_cpu_hash_64(thr_id, throughput, pdata[19], NULL, d_hash[thr_id], order++);
+		TRACE("bmw    :");
 
-		quark_blake512_cpu_hash_80(thr_id, throughput, pdata[19], d_hash[thr_id]);
-		TRACE("blake80:");
-
-		for (int i = 1; i < hashes; i++)
+		for (int i = 0; i < (4 * (HASH_FUNC_COUNT - 2)); i += 4)
 		{
-			const char elem = hashOrder[i];
-			const uint8_t algo64 = elem >= 'A' ? elem - 'A' + 10 : elem - '0';
-
-			switch (algo64) {
-			case BLAKE:
-				quark_blake512_cpu_hash_64(thr_id, throughput, pdata[19], NULL, d_hash[thr_id], i);
-				TRACE("blake  :");
-				break;
-			case BMW:
-				quark_bmw512_cpu_hash_64(thr_id, throughput, pdata[19], NULL, d_hash[thr_id], i);
-				TRACE("bmw    :");
-				break;
+			switch ((sequence >> i) & 0xf) {
 			case GROESTL:
-				quark_groestl512_cpu_hash_64(thr_id, throughput, pdata[19], NULL, d_hash[thr_id], i);
+				quark_groestl512_cpu_hash_64(thr_id, throughput, pdata[19], NULL, d_hash[thr_id], order++);
 				TRACE("groestl:");
 				break;
 			case SKEIN:
-				quark_skein512_cpu_hash_64(thr_id, throughput, pdata[19], NULL, d_hash[thr_id], i);
+				quark_skein512_cpu_hash_64(thr_id, throughput, pdata[19], NULL, d_hash[thr_id], order++);
 				TRACE("skein  :");
 				break;
 			case JH:
-				quark_jh512_cpu_hash_64(thr_id, throughput, pdata[19], NULL, d_hash[thr_id], i);
+				quark_jh512_cpu_hash_64(thr_id, throughput, pdata[19], NULL, d_hash[thr_id], order++);
 				TRACE("jh512  :");
 				break;
 			case KECCAK:
-				quark_keccak512_cpu_hash_64(thr_id, throughput, pdata[19], NULL, d_hash[thr_id], i);
+				quark_keccak512_cpu_hash_64(thr_id, throughput, pdata[19], NULL, d_hash[thr_id], order++);
 				TRACE("keccak :");
 				break;
 			case LUFFA:
-				x11_luffa512_cpu_hash_64(thr_id, throughput, pdata[19], NULL, d_hash[thr_id], i);
+				x11_luffa512_cpu_hash_64(thr_id, throughput, pdata[19], NULL, d_hash[thr_id], order++);
 				TRACE("luffa  :");
 				break;
 			case CUBEHASH:
-				x11_cubehash512_cpu_hash_64(thr_id, throughput, pdata[19], NULL, d_hash[thr_id], i);
+				x11_cubehash512_cpu_hash_64(thr_id, throughput, pdata[19], NULL, d_hash[thr_id], order++);
 				TRACE("cube   :");
 				break;
 			case SHAVITE:
-				x11_shavite512_cpu_hash_64(thr_id, throughput, pdata[19], NULL, d_hash[thr_id], i);
+				x11_shavite512_cpu_hash_64(thr_id, throughput, pdata[19], NULL, d_hash[thr_id], order++);
 				TRACE("shavite:");
 				break;
 			case SIMD:
-				x11_simd512_cpu_hash_64(thr_id, throughput, pdata[19], NULL, d_hash[thr_id], i);
+				x11_simd512_cpu_hash_64(thr_id, throughput, pdata[19], NULL, d_hash[thr_id], order++);
 				TRACE("simd   :");
 				break;
-#if HASH_FUNC_COUNT > 10
-			case ECHO:
-				x11_echo512_cpu_hash_64(thr_id, throughput, pdata[19], NULL, d_hash[thr_id], i);
-				TRACE("echo   :");
-				break;
-#endif
 			}
 		}
 
